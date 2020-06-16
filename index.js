1ff183c0a0f9b00f7f26a75a8ef474fba8336d44
CarolinaMoraes <carolina.moraesjosephik@gmail.com>
2020-06-16 00:15:22 -0300

    🚀: re-configurando deploy

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ const port = process.env.PORT || "3000";
 */
 app.use(express.urlencoded({ extended: false }));
 
+/**
+ * Enabling CORS
+ */
+app.use(function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next();
+
+});
+
 
 /**
  * Routes Definitions

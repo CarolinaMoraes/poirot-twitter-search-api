066c67e81e0de99b09d173a5b5e08bf5ee08352c
CarolinaMoraes <carolina.moraesjosephik@gmail.com>
2020-11-02 21:09:11 -0300

    🚧: retornando perfis dos usuários buscados

--- index.js
+++ index.js
@@ -35,21 +35,33 @@ app.use(function (req, res, next) {
  * Routes Definitions
  */
 
-app.get("/conexao/:usuarioFonte/com/:usuarioAlvo", (req, res) => {
+app.get("/conexao/:usuarioFonte/com/:usuarioAlvo", async (req, res) => {
     const twitter = twitterClient.provideClient();
-    twitter.get("/friendships/show", { source_screen_name: req.params.usuarioFonte, target_screen_name: req.params.usuarioAlvo }, (erro, data) => {
-        if (!erro) {
-            const relacionamento = {
-                fonteSegueAlvo: data.relationship.source.following,
-                alvoSegueFonte: data.relationship.source.followed_by
-            }
-            res.status(200).send(relacionamento);
-        }
-        else {
-            res.status(429).send({ mensagem: "A quantidade máxima de requisições pelo limite de tempo foi alcançada" })
+
+    try {
+        const resposta = await twitterClient.getConexaoEntreContas(twitter, req.params.usuarioFonte, req.params.usuarioAlvo);
+
+        const relacionamento = {
+            fonteSegueAlvo: resposta.relationship.source.following,
+            alvoSegueFonte: resposta.relationship.source.followed_by
         }
 
-    });
+        let userFonte = {};
+        let userAlvo = {};
+
+        // Tenta pegar o usuário fonte e o alvo, mas se não der tudo bem :)
+        try {
+            userFonte = await twitterClient.getUsuario(twitter, resposta.relationship.source.screen_name);
+            userAlvo = await twitterClient.getUsuario(twitter, resposta.relationship.target.screen_name);
+        }
+
+        finally {
+            res.status(200).send({ relacionamento, userFonte, userAlvo });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(error.statusCode).send({ mensagem: error.allErrors[0].message });
+    }
 });
 
 /**
--- twitter/twitterClient.js
+++ twitter/twitterClient.js
@@ -9,5 +9,43 @@ module.exports = {
             access_token_secret: process.env.access_token_secret,
         })
         return T;
+    },
+
+    /**
+     * Retorna um objeto-usuário do twitter
+     * @param {import { Twit } from "twit";} twitterClient 
+     */
+    getUsuario(twitterClient, screenName) {
+        return new Promise((resolve, reject) => {
+
+            twitterClient.get("/users/show", { screen_name: screenName }, (erro, data) => {
+                if (!erro) {
+                    resolve(data);
+                }
+
+                else {
+                    reject(erro);
+                }
+            })
+
+        })
+    },
+
+    /**
+     * Busca se 2 contas se seguem no twitter
+     * @param {import { Twit } from "twit";} twitterClient 
+     */
+    getConexaoEntreContas(twitterClient, nomeFonte, nomeAlvo) {
+        return new Promise((resolve, reject) => {
+            twitterClient.get("/friendships/show", { source_screen_name: nomeFonte, target_screen_name: nomeAlvo }, (erro, data) => {
+                if (!erro) {
+                    resolve(data);
+                }
+
+                else {
+                    reject(erro);
+                }
+            });
+        })
     }
 }
